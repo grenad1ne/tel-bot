49e620be7cccf74926da02327c2db35f9bd54a05
Andrey <dushadryasov@gmail.com>
2021-03-20 03:29:58 +0700

    set sdk

--- index.php
+++ index.php
@@ -1,63 +1,39 @@
 <?php
 
-/*
-* This file is part of GeeksWeb Bot (GWB).
-*
-* GeeksWeb Bot (GWB) is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License version 3
-* as published by the Free Software Foundation.
-* 
-* GeeksWeb Bot (GWB) is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.  <http://www.gnu.org/licenses/>
-*
-* Author(s):
-*
-* © 2015-2018 Kasra Madadipouya <kasra@madadipouya.com>
-*
-*/
 require 'vendor/autoload.php';
 
-$client = new Zelenin\Telegram\Bot\Api('1706737657:AAGwdAYlMkxPqm6p_48eKcNlqugBLVswDsI'); // Set your access token
+use Telegram\Bot\Api;
+
+$telegram = new Api('1706737657:AAGwdAYlMkxPqm6p_48eKcNlqugBLVswDsI');
 $url = 'https://excuse-telegram-bot.herokuapp.com'; // URL RSS feed
-$contents = file_get_contents('php://input');
-$update = json_decode(file_get_contents('php://input'));
-$reasons = file('./reasons.txt');
 
-//your app
-try {
-    $text = $update->message->text;
+$result = $telegram->getWebhookUpdates();
 
-//    if ($text) {
-//        $rand_key = array_rand($reasons, 1);
-//        $response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
-//        $response = $client->sendMessage(
-//            [
-//                'chat_id' => $update->message->chat->id,
-//                'text' => "Друзья, простите. Причина моего проеба: " . $reasons[$rand_key],
-//            ]
-//        );
-//    }
+$text = $result["message"]["text"]; //Текст сообщения
+$chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
+$name = $result["message"]["from"]["username"]; //Юзернейм пользователя
+$keyboard = [["Последние статьи"], ["Картинка"], ["Гифка"]]; //Клавиатура
 
-    if ($text === '/add') {
-//
-        $reasonText = substr($text, stripos($text, '/add') + 5);
-//
-//        $fp = fopen('./reasons.txt', 'a+');
-//        fwrite($fp, $reasonText . PHP_EOL);
-//        fclose($fp);
-
-        $response = $client->sendChatAction(['chat_id' => $update->message->chat->id, 'action' => 'typing']);
-        $response = $client->sendMessage(
-            [
-                'chat_id' => $update->message->chat->id,
-                'text' => "Опа, новая отмазочка для Олега" . $contents,
-            ]
+if ($text) {
+    if ($text == "/start") {
+        $reply = "Добро пожаловать в бота!";
+        $reply_markup = $telegram->replyKeyboardMarkup(
+            ['keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false]
         );
+        $telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup]);
+    } elseif ($text == "/help") {
+        $reply = "Информация с помощью.";
+        $telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply]);
+    } elseif ($text == "Картинка") {
+        $url = "https://68.media.tumblr.com/6d830b4f2c455f9cb6cd4ebe5011d2b8/tumblr_oj49kevkUz1v4bb1no1_500.jpg";
+        $telegram->sendPhoto(['chat_id' => $chat_id, 'photo' => $url, 'caption' => "Описание."]);
+    } elseif ($text == "Гифка") {
+        $url = "https://68.media.tumblr.com/bd08f2aa85a6eb8b7a9f4b07c0807d71/tumblr_ofrc94sG1e1sjmm5ao1_400.gif";
+        $telegram->sendDocument(['chat_id' => $chat_id, 'document' => $url, 'caption' => "Описание."]);
+    } else {
+        $reply = "По запросу \"<b>" . $text . "</b>\" ничего не найдено.";
+        $telegram->sendMessage(['chat_id' => $chat_id, 'parse_mode' => 'HTML', 'text' => $reply]);
     }
-} catch (\Zelenin\Telegram\Bot\NotOkException $e) {
-    //echo error message ot log it
-    //echo $e->getMessage();
-
+} else {
+    $telegram->sendMessage(['chat_id' => $chat_id, 'text' => "Отправьте текстовое сообщение."]);
 }
