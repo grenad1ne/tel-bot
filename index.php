6d51b688271031f63a47537e2c62535f4408aabf
Andrey <dushadryasov@gmail.com>
2021-03-20 03:37:06 +0700

    set sdk

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $result = $telegram->getWebhookUpdates();
 $text = $result["message"]["text"]; //Текст сообщения
 $chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
 $name = $result["message"]["from"]["username"]; //Юзернейм пользователя
-$keyboard = [["Последние статьи"], ["Картинка"], ["Гифка"]]; //Клавиатура
+$keyboard = [["Срочно нужна причина для отмазки"]]; //Клавиатура
 
 if ($text) {
     if ($text == "/start") {
@@ -22,17 +22,8 @@ if ($text) {
             ['keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false]
         );
         $telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup]);
-    } elseif ($text == "/help") {
-        $reply = "Информация с помощью.";
-        $telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply]);
-    } elseif ($text == "Картинка") {
-        $url = "https://68.media.tumblr.com/6d830b4f2c455f9cb6cd4ebe5011d2b8/tumblr_oj49kevkUz1v4bb1no1_500.jpg";
-        $telegram->sendPhoto(['chat_id' => $chat_id, 'photo' => $url, 'caption' => "Описание."]);
-    } elseif ($text == "Гифка") {
-        $url = "https://68.media.tumblr.com/bd08f2aa85a6eb8b7a9f4b07c0807d71/tumblr_ofrc94sG1e1sjmm5ao1_400.gif";
-        $telegram->sendDocument(['chat_id' => $chat_id, 'document' => $url, 'caption' => "Описание."]);
     } else {
-        $reply = "По запросу \"<b>" . $text . "</b>\" ничего не найдено.";
+        $reply = "Тупо тыкай кнопку. Здесь нет дополнительного функционала";
         $telegram->sendMessage(['chat_id' => $chat_id, 'parse_mode' => 'HTML', 'text' => $reply]);
     }
 } else {
